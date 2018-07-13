05f898818124c19ec278af749b090b14f1d9de84
Beth Tseng <a5520592@gmail.com>
2018-07-13 09:39:19 +0800

    update training server url

--- web_example.py
+++ web_example.py
@@ -1,12 +1,12 @@
 import holdem
 import agent
 
-SERVER_URI = "ws://poker-training.vtr.trendnet.org:3001/" # beta
-# SERVER_URI = r"ws://allhands2018-training.dev.spn.a1q7.net:3001" # training
+#SERVER_URI = "ws://poker-training.vtr.trendnet.org:3001/" # beta
+SERVER_URI = "ws://http://poker-dev.wrs.club:3001/" # training
 
 # name="Enter Your Name Here"
 name="Beth"
-model = agent.allRaiseModel()
+model = agent.MontecarloModel()
 
 # while True: # Reconnect after Gameover
 client_player = holdem.ClientPlayer(SERVER_URI, name, model, debug=True, playing_live=True)

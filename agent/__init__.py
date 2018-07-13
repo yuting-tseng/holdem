c916e8af50529aedac7987c1b9f7b147c04686ba
Beth Tseng <a5520592@gmail.com>
2018-07-13 23:46:48 +0800

    DQNagent init

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,8 +1,9 @@
 from .idiotAgent import idiotModel
 from .debugAgent import debugModel
-from .dqn_agent import dqnModel
+# from .dqn_agent import dqnModel
 from .allfoldAgent import allFoldModel
 from .allcallAgent import allCallModel
 from .allinAgent import allinModel
 from .allraiseAgent import allRaiseModel
 from .montecarlo_agent import MontecarloModel
+from .DQNagent import dqnModel
